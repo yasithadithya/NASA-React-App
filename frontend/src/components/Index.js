6f180b7fb73b1bb75a2e7b2f3ad8b1e97aae9c4b
Yasith <it21818516@my.sliit.lk>
2024-05-03 17:36:09 +0530

    Index Page Changes

--- frontend/src/components/Index.js
+++ frontend/src/components/Index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import Button from '@mui/material/Button';
 
 function Index() {
   return (
     <>
       <Header />
       <div className="container-fluid" id="hero">
-        <div className="jumbotron hero-section p-5">
+        <div className="jumbotron hero-section p-5" style={{ padding: '5rem' }}>
           <h1 className="display-4">Welcome to Xplore</h1>
-          <p className="lead">Here you will find great pictures and 3D models of the solar system, but first:<br/> <strong>Check out NASA's picture of the day down below!</strong> </p>
-          <Button variant="contained" color="primary" style={{ backgroundColor: '#6232A6' }} href="/pic" size="large">
-            Picture of the Day
-          </Button>
+          <p className="lead">Embark on a cosmic journey through the wonders of our solar system with Xplore! Immerse yourself in breathtaking imagery and captivating 3D models that unveil the mysteries of space.</p>
         </div>
       </div>
       <Footer />
@@ -22,3 +18,4 @@ function Index() {
 }
 
 export default Index;
+
